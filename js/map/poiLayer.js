c1899027787d9aa7ce7a55116a8e0d36ab52e84d
Delta Omni <108251661+deltaomni@users.noreply.github.com>
2025-12-28 10:53:10 -0300

    Update poiLayer.js

--- js/map/poiLayer.js
+++ js/map/poiLayer.js
@@ -1,28 +1,11 @@
 import { eventBus } from '../core/eventBus.js';
-import { cityStore } from '../core/cityStore.js';
+import { getCity } from '../core/cityStore.js';
 
 let map;
 let activeCard = null;
 
-function buildPOI(negocio, categoria) {
-    return `
-    <div class="property ${negocio.premium ? 'premium' : ''}"
-         style="--poi-color: ${categoria.color}">
-         
-        <div class="icon">
-            <i class="fa fa-${categoria.icon}"></i>
-        </div>
-
-        <div class="details">
-            <div class="price fw-bold">${negocio.nome}</div>
-            <div class="address">${negocio.endereco}</div>
-        </div>
-    </div>
-    `;
-}
-
 function addBusinessPOIs() {
-    const city = cityStore.getCity();
+    const city = getCity();
     if (!city || !city.negocios) return;
 
     const categorias = city.categorias || {};
@@ -75,10 +58,6 @@ function addBusinessPOIs() {
         });
 }
 
-/* =========================
-   EVENTOS
-========================= */
-
 eventBus.on('map:ready', leafletMap => {
     map = leafletMap;
 });
@@ -86,3 +65,5 @@ eventBus.on('map:ready', leafletMap => {
 eventBus.on('city:loaded', () => {
     addBusinessPOIs();
 });
+
+
