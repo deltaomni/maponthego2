75e8bf1eb0a5999ce6e43bb80a3fd8f040aaacde
Delta Omni <108251661+deltaomni@users.noreply.github.com>
2025-12-30 12:27:28 -0300

    Update modalController.js

--- js/modal/modalController.js
+++ js/modal/modalController.js
@@ -1,7 +1,20 @@
 import { eventBus } from '../core/eventBus.js';
-import { openSiteModal } from './modalController.js'; // ajuste se necessário
 
+/**
+ * Abre o site do negócio no modal
+ * (implementação provisória)
+ */
+export function openSiteModal({ citySlug, businessSlug }) {
+    console.log('[modalController] openSiteModal', citySlug, businessSlug);
 
+    // por enquanto, apenas redireciona
+    const url = `/negocio/index.html?city=${citySlug}&site=${businessSlug}`;
+    window.location.href = url;
+}
+
+/**
+ * Escuta o contexto do negócio
+ */
 eventBus.on('business:context', (context) => {
     if (context.source === 'url' && context.businessSlug) {
         console.log('[modalController] abrindo site demo:', context);
