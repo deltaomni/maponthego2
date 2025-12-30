b6ab57d66183e723e24db8c32b0da6ba7aead5e6
Delta Omni <108251661+deltaomni@users.noreply.github.com>
2025-12-30 17:09:37 -0300

    Update negocio.js

--- negocio/negocio.js
+++ negocio/negocio.js
@@ -1,2 +1,6 @@
-import '../js/core/businessStore.js';
-import '../js/modal/siteRenderer.js';
+//import '../js/core/businessStore.js';
+//import '../js/modal/siteRenderer.js';
+
+eventBus.on('business:data', ({ city, business }) => {
+    renderFullSite({ city, business });
+});
